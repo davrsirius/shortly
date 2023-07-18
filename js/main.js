414d8397b232ff49056f1359f034add326ac6dfd
davrsirius <davrsirius@gmail.com>
2023-07-18 23:27:32 +0500

    Add and correct class for javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const elUrlShortenerForm = document.querySelector('.url-shortener-form');
+const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
 
 const elUrlShortenerResults = document.querySelector('.url-shortener__results');
 
